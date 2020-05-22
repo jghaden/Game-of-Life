d9fa7cafb8fc5da47bc59a66157b2e2f595d6bfc
Joshua Haden <joshghaden@gmail.com>
2020-05-21 22:56:14 -0500

    Setup basic simulation with random initialization.

--- GameOfLife/src/main.cpp
+++ GameOfLife/src/main.cpp
@@ -4,9 +4,11 @@
 
 int main(void)
 {
+	SetWindowSize(680, 695);
+	SetFontSize(12, 12);
 	ShowConsoleCursor(false);
 
-	Game game(80, 25);
+	Game game(80, 40);
 
 	while (true)
 	{
@@ -15,21 +17,23 @@ int main(void)
 		//game.SetState(40, 11, CELL_ALIVE);
 		//game.SetState(41, 11, CELL_ALIVE);
 
-		// Plus
-		game.SetState(40, 10, CELL_ALIVE);
+		//// Plus
+		//game.SetState(40, 10, CELL_ALIVE);
 
-		game.SetState(39, 11, CELL_ALIVE);
-		game.SetState(40, 11, CELL_ALIVE);
-		game.SetState(41, 11, CELL_ALIVE);
+		//game.SetState(39, 11, CELL_ALIVE);
+		//game.SetState(40, 11, CELL_ALIVE);
+		//game.SetState(41, 11, CELL_ALIVE);
 
-		game.SetState(40, 12, CELL_ALIVE);
+		//game.SetState(40, 12, CELL_ALIVE);
 
+		game.Random();
 
-		for (int i = 0; i < 2000; i++)
+		for (int i = 0; i < 100; i++)
 		{
 			game.ShowMap();
-			std::cout << game.GetNeighbors(43, 11);
-			getchar();
+
+			//getchar();
+
 			game.SimulationStep();
 		}
 
