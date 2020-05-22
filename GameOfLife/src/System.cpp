5145fa35d726235f7abb7d6d9d9122be4045ca73
Joshua Haden <joshghaden@gmail.com>
2020-05-21 22:54:00 -0500

    Added functions to set window and font size.

--- GameOfLife/src/System.cpp
+++ GameOfLife/src/System.cpp
@@ -18,3 +18,24 @@ void ShowConsoleCursor(bool showFlag)
 	cursorInfo.bVisible = showFlag;
 	SetConsoleCursorInfo(out, &cursorInfo);
 }
+
+void SetWindowSize(int w, int h)
+{
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, w, h, TRUE);
+}
+
+void SetFontSize(int w, int h)
+{
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = w;
+	cfi.dwFontSize.Y = h;
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	wcscpy_s(cfi.FaceName, L"Consolas");
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
