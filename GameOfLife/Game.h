5ad0db7c872446166c38bc9555f9e7b7414515c2
Joshua Haden <joshghaden@gmail.com>
2020-05-21 21:14:14 -0500

    Added Game functions.

---
+++ GameOfLife/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "PCH.h"
+
+class Game
+{
+public:
+	Game(unsigned width, unsigned height);
+
+	void Reset();
+	void Random();
+	void SimulationStep();
+	void ShowMap();
+
+	// Getters
+	bool GetState(unsigned int x, unsigned int y);
+	unsigned int GetWidth();
+	unsigned int GetHeight();
+	unsigned int GetNeighbors(unsigned int x, unsigned int y);
+
+	// Setters
+	void SetState(unsigned int x, unsigned int y, bool state);
+protected:
+	unsigned width, height;
+
+	std::vector<std::vector<bool>> cellMap;
+
+	void Init();
+};
